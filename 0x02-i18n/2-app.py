892c9692d03e4652dd82baad8622bc531deb7969
GracyDinma <okohagchidinma@gmail.com>
2024-11-08 08:51:17 +0000

    task 2 updated

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,51 +1,45 @@
 #!/usr/bin/env python3
-"""A basic Flask web app serves a simple HTML page.
-"""
+'''Task 2: Get locale from request
+'''
 
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-# config class to store language and timezone
-class config:
-    """_summary_
+class Config:
+    '''Config class'''
 
-    Returns:
-        _type_:_description_
-    """
-    LANGUAGES = ['en', 'fr']
-    DEFAULT_LOCALE = 'en'
-    DEFAULT_TIMEZONE = 'UTC'
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# configure the flask app
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """_summary_
+    """Retrieves the locale for a web page.
 
     Returns:
-        _type_:_description_
+        str: best match
     """
-    return request.accept.languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
-    """default route
+    '''default route
 
     Returns:
         html: homepage
-    """
-    return render_template('2-index.html')
+    '''
+    return render_template("2-index.html")
 
 
-if __name__ == '__main__':
-    # Run the application
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run()
