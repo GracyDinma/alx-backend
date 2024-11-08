b7e1d080011f79503c157c6812d4299ece193bf2
GracyDinma <okohagchidinma@gmail.com>
2024-11-08 09:18:51 +0000

    4 updated

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,44 +1,53 @@
 #!/usr/bin/env python3
-# Setting up a basic flask app.
-from flask import Flask, render_template
-from flask_babel import Babel, gettext
+'''Task 4: Force locale with URL parameter
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
-# flask constructor
+class Config:
+    '''Config class'''
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
-
-
-# config class to store language and timezone
-class config:
-    # Setting languages
-    LANGUAGES = ['en', 'fr']
-    DEFAULT_LOCALE = 'en'
-    DEFAULT_TIMEZONE = 'UTC'
-
-
-# Set up app's configuration with config class
-app.config.from_object(config)
-
-# Instantiate the Babel object
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
-# creating a get locale function with babel.localselector
+
 @babel.localeselector
-def get_locale():
-    # Check if hte 'locale' parameter is in hte query string
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
     locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    if locale in app.config['LANGUAGES']:
         return locale
-    # Use the parameter to find the best match for the user's language.
-    return request.accept.languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# Define a route for the root URL
 @app.route('/')
-def index():
-    return render_template('4-index.html')
+def index() -> str:
+    '''default route
+
+    Returns:
+        html: homepage
+    '''
+    return render_template("4-index.html")
+
+# uncomment this line and comment the @babel.localeselector
+# you get this error:
+# AttributeError: 'Babel' object has no attribute 'localeselector'
+# babel.init_app(app, locale_selector=get_locale)
 
 
-# Running the application
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
